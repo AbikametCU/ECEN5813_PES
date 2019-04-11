e2375e5bbaed3b9309340f2ae43e8f677005f6b9
AydBlot <46939500+AydBlot@users.noreply.github.com>
2019-04-11 16:12:41 -0600

    Unit Tests and .c and .h files

---
+++ Circular Buffers/ring.c
@@ -0,0 +1,74 @@
+/*========================================================================
+** Circular buffer
+** ECEN5813
+**========================================================================*/
+#include <stdlib.h>
+#include <stdio.h>
+#include "ring.h"
+
+ring_t *Ring_init( int length ){
+    //Initialize a structure to return
+    ring_t *RingPTR = malloc(sizeof(ring_t));
+   
+    //allocate memory for the buffer
+    char *buff = (char *)malloc(length * sizeof(char));
+    
+    //Populate the struct and return it
+    RingPTR->Length = length;
+    RingPTR->Ini = 0;
+    RingPTR->Outi = 0;
+    RingPTR->Buffer=buff;
+    
+    return RingPTR;
+}
+int inPlus(int in, ring_t *ring){
+    return (in > ring->Length ? 0 : in++);
+}
+
+int insert( ring_t *ring, char data ){
+    //if(inPlus(ring->Ini, ring) == ring->Outi){
+      //  printf("Buffer is full, please remove an element");
+       // return -1;
+   // }
+    if(ring->Ini>=ring->Length ){
+//            printf("Buffer is full\n");
+    	ring->Ini = 0;
+		return -1;
+    }
+    else{
+        ring->Buffer[ring->Ini] = data;
+        ring->Ini++;
+//    	printf("Added\n");
+    }    
+    return 0;
+    
+}
+
+int remove_element( ring_t *ring, char *data ){
+    if(ring->Ini == ring->Outi){
+//        printf("cannot remove an item from an empty buffer\n");
+        return -1;
+    }
+    else{
+        *data = ring->Buffer[ring->Outi];
+        ring->Outi++;
+        if(ring->Outi >= ring->Length){
+			ring->Outi=0;
+		}
+//        printf("Item successfully removed\n");
+        if(ring->Ini>=ring->Length ){
+            ring->Ini=0;
+//            printf("Ini back to 0\n");
+        }
+        return 0;
+    }
+}
+
+int entries( ring_t *ring ){
+    if ( (ring->Ini-ring->Outi) < 0 ){
+        return ( (ring->Ini-ring->Outi) + ring->Length);
+    }
+    else{
+        return (ring->Ini-ring->Outi);
+    }    
+}
---
+++ Circular Buffers/ring_test.c
@@ -0,0 +1,76 @@
+/*========================================================================
+** ring_test.c
+** Circular buffer testing
+** ECEN5813
+**========================================================================*/
+#include <CUnit/CUnit.h>	//ASSERT macros for use in test cases, and includes other framework headers.
+#include <CUnit/CUError.h>	//Error handing functions and data types. Included automatically by CUnit.h.
+#include <CUnit/TestDB.h>	//Data type definitions and manipulation functions for the test registry, suites, and tests. Included automatically by CUnit.h.
+#include <CUnit/TestRun.h>	//Data type definitions and functions for running tests and retrieving results. Included automatically by CUnit.h.
+#include <CUnit/Automated.h>	//Automated interface with xml output.
+#include <CUnit/Basic.h>	//Basic interface with non-interactive output to stdout.
+#include <CUnit/Console.h>	//Interactive console interface.
+//#include <CUnit/CUCurses.h>	//Interactive console interface (*nix).
+//#include <CUnit/Win.h>	Windows interface (not yet implemented).
+
+
+#include "ring.h"
+
+ring_t *ring_buffer;
+int test_init(void)
+{ 
+  	ring_buffer = init(30);
+	return 0;
+}
+
+void test_insert(void){	
+    for (int i = 0; i<29; i++){   
+    	CU_ASSERT(insert(ring_buffer, 'a'+i)==0);
+	printf("%d\n", i);
+    }	
+    CU_ASSERT(entries(ring_buffer)==29);
+    char data;
+    CU_ASSERT(remove_element(ring_buffer,&data)==0);
+    CU_ASSERT(insert(ring_buffer, 'a')==0);
+    printf("data removed:%c\n", data);
+
+}
+
+void test_remove(void){
+    char data;
+    CU_ASSERT(insert(ring_buffer, 'a')==0);
+    CU_ASSERT(remove_element(ring_buffer,&data)==0);
+    CU_ASSERT(entries(ring_buffer)==0);
+    printf("data:%c\n", data);
+}
+
+int clean_suite(void){
+return 0;
+}
+
+int main(void){
+    //test_init(); 
+    CU_pSuite pSuite = NULL;
+
+   /* initialize the CUnit test registry */
+   if (CUE_SUCCESS != CU_initialize_registry()){
+    	 return CU_get_error();
+   }
+   pSuite = CU_add_suite("Suite_1", test_init, clean_suite);
+   if (NULL == pSuite){
+	   CU_cleanup_registry();
+	   return CU_get_error();
+   }
+     /* add the tests to the suite */
+   if ((NULL == CU_add_test(pSuite, "test of remove()", test_remove)) || (NULL == CU_add_test(pSuite, "test of insert()", test_insert)))
+   {
+      CU_cleanup_registry();
+      return CU_get_error();
+   }
+    printf("test2\n");
+      /* Run all tests using the CUnit Basic interface */
+    CU_basic_set_mode(CU_BRM_VERBOSE);
+    CU_basic_run_tests();
+    CU_cleanup_registry();
+    return CU_get_error();
+}
