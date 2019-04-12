74fbc3f36fc4fe9a0724e184a0c50ad7cfc29109
Abikamet <abikamet.nathan@gmail.com>
2019-04-12 00:00:25 -0600

    randomized unit testing added

--- Circular Buffers/ring_test.c
+++ Circular Buffers/ring_test.c
@@ -12,7 +12,7 @@
 #include <CUnit/Console.h>	//Interactive console interface.
 //#include <CUnit/CUCurses.h>	//Interactive console interface (*nix).
 //#include <CUnit/Win.h>	Windows interface (not yet implemented).
-
+#include <time.h>
 
 #include "ring.h"
 
@@ -45,22 +45,69 @@ void test_remove(void){
 }
 
 void test_fullBuffer(void){
-   ring_t *ring_buffer2;
-   ring_buffer2 = Ring_init(30);
+   //ring_t *ring_buffer2;
+   ring_buffer = Ring_init(30);
    int i=0;
-   for (i=0;i<ring_buffer2->Length;i++){
-		insert(ring_buffer2,i+97);
+   for (i=0;i<ring_buffer->Length;i++){
+		CU_ASSERT(insert(ring_buffer, i+97)==0);
 	}//Buffer should be full now
-	 CU_ASSERT(-1 == insert(ring_buffer2, 'a'));	
+	 CU_ASSERT(-1 == insert(ring_buffer, 'a'));	
 }
 
 void test_emptyBuffer(void){
-   ring_t *ring_buffer2;
-   ring_buffer2 = Ring_init(30);
+   //ring_t *ring_buffer4;
+   ring_buffer = Ring_init(30);
    char data;
-   CU_ASSERT(remove_element(ring_buffer2,&data)==-1);
+   CU_ASSERT(remove_element(ring_buffer,&data)==-1);
+   
 }
 
+
+void randomized_testing(void){
+	//ring_t *ring_buffer3;
+        ring_buffer = Ring_init(400);
+	srand(time(0));
+	int no_of_random_operations=100;
+	int current_no_of_entries=0;
+	int i,rand_value;
+	char rand_char_ascii;
+	for(i=0;i<no_of_random_operations;i++){
+		printf("Current No of Entries:%d\n",current_no_of_entries);
+		rand_value=rand()%4;
+		rand_char_ascii=97+(rand()%26);//chooses random lowercase letter
+		switch(rand_value){
+			case 1 ... 2:{//Insert
+				printf("Insert\n");
+				if(current_no_of_entries<ring_buffer->Length){			
+					CU_ASSERT(insert(ring_buffer, rand_char_ascii)==0);
+					current_no_of_entries++;
+				}
+				else{
+					CU_ASSERT(insert(ring_buffer, &rand_char_ascii)==-1);				
+				} 
+			}break;
+			
+			case 3:{//Remove
+				printf("Remove\n");
+				if(current_no_of_entries>0){
+					CU_ASSERT(insert(ring_buffer, rand_char_ascii)==0);
+					current_no_of_entries--;
+				}
+				else{
+					
+					CU_ASSERT(remove_element(ring_buffer,&rand_char_ascii)==-1);
+									
+				}
+
+			}break;
+		
+			case 4:{//entries
+				printf("Entries\n");
+				CU_ASSERT(entries(ring_buffer)==current_no_of_entries);
+			}break;
+		}
+	}
+}
 
 
 int clean_suite(void){
@@ -81,8 +128,9 @@ int main(void){
 	   return CU_get_error();
    }
      /* add the tests to the suite */
-   if ((NULL == CU_add_test(pSuite, "test of remove()", test_remove)) || (NULL == CU_add_test(pSuite, "test of insert()", test_insert)) || (NULL == CU_add_test(pSuite, "Testing full buffer Insert", test_fullBuffer)) || (NULL == CU_add_test(pSuite, "Testing empty  buffer remove",test_emptyBuffer)))
+   if ((NULL == CU_add_test(pSuite, "test of remove()", test_remove)) || (NULL == CU_add_test(pSuite, "test of insert()", test_insert)) || (NULL == CU_add_test(pSuite, "Testing full buffer Insert", test_fullBuffer)) || (NULL == CU_add_test(pSuite, "Testing empty  buffer remove",test_emptyBuffer)) || (NULL == CU_add_test(pSuite, "Randomized testing",randomized_testing)))
    {
+
       CU_cleanup_registry();
       return CU_get_error();
    }
